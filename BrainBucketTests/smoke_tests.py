69c49937309a9ac487fff39df03b9833e49af8aa
Nadezda74 <nadezdachesnakova@gmail.com>
2021-12-20 20:50:09 -0500

    exercise2webdriverwait

--- BrainBucketTests/smoke_tests.py
+++ BrainBucketTests/smoke_tests.py
@@ -1,14 +1,19 @@
 from selenium import webdriver
 
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+
+
 driver = webdriver.Chrome(executable_path = "c:\selenium browser drivers\chromedriver.exe")
 
 driver.get("https://cleveronly.com/brainbucket/index.php?route=account/login")
 
 email_field = driver.find_element_by_name("email")
-email_field.send_keys("mashka@gmail.com")
+email_field.send_keys("Nikitos@gmail.com")
 
 password_field = driver.find_element_by_name("password")
-password_field.send_keys("ulianka88")
+password_field.send_keys("Chelyabinsk74")
 
-login_button = driver.find_element_by_xpath("//body//div/div[2]/div/form/input")
-login_button.click()
+login_field = WebDriverWait(driver,10).until(EC.element_to_be_clickable((By.XPATH,"//body//div[2]//form/input")))
+login_field.click()
